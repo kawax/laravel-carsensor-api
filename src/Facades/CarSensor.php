ade0c7ecec563b43179a964bad1c77facd9343bc
kawax <kawaxbiz@gmail.com>
2019-02-28 00:05:51 +0900

    facade

--- src/Facades/CarSensor.php
+++ src/Facades/CarSensor.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Facade;
 
 use Revolution\CarSensor\Contracts\Factory;
 
-class CarSensorFacade extends Facade
+class CarSensor extends Facade
 {
     /**
      * Get the registered name of the component.
